d69d993a0b2100d070651062af2373c7e29b4d33
ivy-kurniawan <ikurniaw@pratt.edu>
2025-02-25 14:15:14 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,195 @@
+var stitch1 = document.getElementById ("stitch1")
+var text1 = document.getElementById ("text1")
+var text2 = document.getElementById ("text2")
+var text3 = document.getElementById ("text3")
+var question1 = document.getElementById ("question1")
+var question2 = document.getElementById ("question2")
+var question3 = document.getElementById ("question3")
+var question4 = document.getElementById ("question4")
+var question5 = document.getElementById ("question5")
+var body = document.getElementById ("body")
+
+stitch1.addEventListener("click", function showtext() {
+  stitch1.style.opacity = "1";
+  console.log("stitch1")
+  text1.style.opacity = "1";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "1";
+  question2.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+body.addEventListener("click", function showstitch() {
+  stitch1.style.opacity = "1";
+  console.log("title")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+//
+
+var stitch2 = document.getElementById ("stitch2")
+var text1 = document.getElementById ("text1")
+var text2 = document.getElementById ("text2")
+var text3 = document.getElementById ("text3")
+var body = document.getElementById ("body")
+
+stitch2.addEventListener("click", function showtext() {
+  stitch2.style.opacity = "1";
+  console.log("stitch2")
+  text1.style.opacity = "0";
+  text2.style.opacity = "1";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "1";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+body.addEventListener("click", function showstitch() {
+  stitch2.style.opacity = "1";
+  console.log("title")
+  text1.style.opacity = "0";
+  question2.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+//
+
+var stitch3 = document.getElementById ("stitch3")
+var text1 = document.getElementById ("text1")
+var text2 = document.getElementById ("text2")
+var text3 = document.getElementById ("text3")
+var text4 = document.getElementById ("text4")
+var body = document.getElementById ("body")
+
+stitch3.addEventListener("click", function showtext() {
+  stitch3.style.opacity = "1";
+  console.log("stitch3")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "1";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "1";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+body.addEventListener("click", function showstitch() {
+  stitch3.style.opacity = "1";
+  console.log("title")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+//
+
+var stitch4 = document.getElementById ("stitch4")
+var text1 = document.getElementById ("text1")
+var text2 = document.getElementById ("text2")
+var text3 = document.getElementById ("text3")
+var text4 = document.getElementById ("text4")
+var body = document.getElementById ("body")
+
+stitch4.addEventListener("click", function showtext() {
+  stitch4.style.opacity = "1";
+  console.log("stitch4")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "1";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "1";
+  question5.style.opacity = "0";
+});
+
+body.addEventListener("click", function showstitch() {
+  stitch4.style.opacity = "1";
+  console.log("title")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
+
+//
+
+var stitch5 = document.getElementById ("stitch5")
+var text1 = document.getElementById ("text1")
+var text2 = document.getElementById ("text2")
+var text3 = document.getElementById ("text3")
+var text4 = document.getElementById ("text4")
+var text5 = document.getElementById ("text5")
+var body = document.getElementById ("body")
+
+stitch5.addEventListener("click", function showtext() {
+  stitch5.style.opacity = "1";
+  console.log("stitch5")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "1";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "1";
+});
+
+body.addEventListener("click", function showstitch() {
+  stitch5.style.opacity = "1";
+  console.log("title")
+  text1.style.opacity = "0";
+  text2.style.opacity = "0";
+  text3.style.opacity = "0";
+  text4.style.opacity = "0";
+  text5.style.opacity = "0";
+  question1.style.opacity = "0";
+  question2.style.opacity = "0";
+  question3.style.opacity = "0";
+  question4.style.opacity = "0";
+  question5.style.opacity = "0";
+});
